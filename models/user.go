8891c9f350ffb2014986d45a99c386bae67d182d
Will Rowston <willrowston@gmail.com>
2020-08-25 10:53:48 -0400

    updated model

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,10 +21,36 @@ func GetUsers() []*User {
 }
 
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("New User must not include id or it must be set...")
+	}
+
 	u.ID = nextID
 	nextID++
 	// &u takes the address of the pointer because
 	// users collection stores pointers to user objects
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+func RemoveUSerByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("User with ID '%v' not found", id)
+
 }
